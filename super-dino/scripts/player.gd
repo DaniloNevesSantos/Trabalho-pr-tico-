56794f47b82976dd788508b968d6a05610aba15d
DaniloNevesSantos <danilopaulosantos9@gmail.com>
2026-09-17 13:48:44 -0300

    personagem 02.1 erro

--- super-dino/scripts/player.gd
+++ super-dino/scripts/player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 10000.0
 const JUMP_FORCE = -400.0
 
+@onready var animação = $Anim
 
 func _physics_process(delta: float) -> void:
+	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -16,10 +19,19 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	
+	if Input.is_action_pressed("ui_left"):
+		
+		velocity.x = delta * SPEED
+	
+		animação.play("run in")
+
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = delta * SPEED
+		animação.play("run")
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		animação.play("stop")
 	move_and_slide()
+	
